023ea6d20b1df3a4791cb917796c8b9e73d64f80
M2K-5F <ivan.semjonow15@gmail.com>
2025-05-07 12:07:23 +0300

    fetch api to html

--- api.py
+++ api.py
@@ -1,7 +1,15 @@
 """api для решение уравнения"""
-from fastapi import FastAPI
+from fastapi import FastAPI, middleware
 from main import kv_ur
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/kvur/")
